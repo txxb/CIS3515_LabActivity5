4bacefbbafcd1922bc9b18000614327195caa272
Maab Taha <tmaab13@gmail.com>
2025-02-19 16:30:50 -0500

    better fix - using the original selected item position & doesn't crash when removing last item

--- app/src/main/java/edu/temple/namelist/MainActivity.kt
+++ app/src/main/java/edu/temple/namelist/MainActivity.kt
@@ -37,9 +37,17 @@ class MainActivity : AppCompatActivity() {
         }
 
         findViewById<View>(R.id.deleteButton).setOnClickListener {
-            (names as MutableList).removeAt(spinner.selectedItemPosition)
-            (spinner.adapter as BaseAdapter).notifyDataSetChanged()
-            nameTextView.text = spinner.selectedItem.toString()
+            val selectedPosition = spinner.selectedItemPosition
+
+            if (selectedPosition >= 0) {
+                (names as MutableList).removeAt(selectedPosition)
+                (spinner.adapter as BaseAdapter).notifyDataSetChanged()
+                spinner.setSelection(0)
+
+                nameTextView.text = if (names.isNotEmpty()) spinner.selectedItem.toString()
+                    else "Nothing to delete!"
+
+            }
         }
 
     }
